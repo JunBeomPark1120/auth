94de39265459319d59b6c344c117d96f372f9a42
junbeompark <a01077348062@gmail.com>
2023-08-29 15:09:11 +0900

    comment create 구현

--- articles/forms.py
+++ articles/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Article
+from .models import Article, Comment
 
 class ArticleForm(forms.ModelForm):
     # title = forms.CharField(
@@ -18,3 +18,9 @@ class ArticleForm(forms.ModelForm):
             'title':forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        # fields = '__all__'
+        fields = ('content',)
