553ea3f78b78acdfeb2d80d754a69c8dc1b2327e
junbeompark <a01077348062@gmail.com>
2023-08-29 13:33:34 +0900

    django-bootstrap-v5 적용/ html 통일화

--- accounts/views.py
+++ accounts/views.py
@@ -5,11 +5,16 @@ from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('accounts:signup')
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    
     else:
         form = CustomUserCreationForm()
         
@@ -17,16 +22,21 @@ def signup(request):
         'form': form,
     }
     
-    return render(request, 'signup.html', context)
+    return render(request, 'account_form.html', context)
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    
     if request.method == 'POST':
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
             next_url = request.GET.get('next') # => /articles/create/
-            return redirect(next_url or 'articles:login')
-        
+
+            return redirect(next_url or 'articles:index')
+            # next 인자가 url에 있을 때 => '/articles/create/' or 'articles:index'
+            # next 인자가 url에 없을 때 => None or 'articles:index'
     else:
         form = CustomAuthenticationForm()
         
@@ -34,7 +44,7 @@ def login(request):
         'form': form,
     }
     
-    return render(request, 'login.html', context)
+    return render(request, 'account_form.html', context)
 
 def logout(request):
     auth_logout(request)
--- auth/settings.py
+++ auth/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'articles'
+    'articles',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
